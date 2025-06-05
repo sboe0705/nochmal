c6194cb4fa379a61805346bec65fad14c9c5af1e
Sebastian Böhm <mail@sboe0705.de>
2025-06-05 05:18:30 +0200

    improved output of build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # clean
+echo "[INFO]  Cleanup distribution directory"
 rm -rf dist
 
 # build
@@ -8,6 +9,7 @@ for goos in windows linux; do
   for goarch in amd64 arm64; do
     outfile="nochmal-${goos}-${goarch}"
     [[ "$goos" == "windows" ]] && outfile+=".exe"
+    echo "[INFO]  Building $outfile"
     GOOS=$goos GOARCH=$goarch go build -o "dist/$outfile" .
   done
 done
